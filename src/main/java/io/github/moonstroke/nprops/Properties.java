8853dd2299435400b54e02f73e3d944ba0e03e6a
Joachim "Moonstroke" MARIE <jiema@live.fr>
2024-03-08 22:06:37 +0100

    Add Properties class
    The class contains the prototypes of all the main methods, to define a
    clear outline for the class. They all simply throw a "not implemented"
    exception.

---
+++ src/main/java/io/github/moonstroke/nprops/Properties.java
@@ -0,0 +1,29 @@
+package io.github.moonstroke.nprops;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Properties {
+
+	public void load(InputStream inputStream) throws IOException {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
+
+	public void store(OutputStream outputStream) throws IOException {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
+
+
+	public void setProperty(String key, String value) {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
+
+	public String getProperty(String key) {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
+
+	public String getProperty(String key, String defaultValue) {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
+}
