d516d4c726e20715f31854ddc4ebd869fe7bab40
Moonstroke <jiema@live.fr>
2024-03-31 00:36:56 +0100

    Add utility functions to the base test class
    The functions allow to return a String representation of the properties
    in the test instance, or to load properties into the instace from a
    String. These functions leverage the Properties.store and .load
    respectively, thus allowing in a single statement to call each method
    while avoiding the String to InputStream or OutputStream conversion and
    I/O exception handling boilerplate.

--- src/test/java/io/github/moonstroke/nprops/test/BaseNpropsPropertiesTest.java
+++ src/test/java/io/github/moonstroke/nprops/test/BaseNpropsPropertiesTest.java
@@ -1,5 +1,13 @@
 package io.github.moonstroke.nprops.test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.BeforeEach;
 
 import io.github.moonstroke.nprops.Properties;
@@ -11,5 +19,26 @@ class BaseNpropsPropertiesTest {
 	@BeforeEach
 	void initProperties() {
 		properties = new Properties();
+	}
+
+
+	/* Utility functions to convert between the Properties instance and Strings */
+
+	protected final void loadFromString(String str) {
+		try (InputStream inputStream = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8))) {
+			properties.load(inputStream);
+		} catch (IOException ioe) {
+			fail(ioe);
+		}
+	}
+
+	protected final String storeToString(String comments) {
+		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+			properties.store(outputStream, comments);
+			return outputStream.toString(StandardCharsets.UTF_8);
+		} catch (IOException ioe) {
+			fail(ioe);
+			return null; /* Not reachable; added to please the compiler */
+		}
 	}
 }
