16eece8e13e0ddc04787a609a49c8c88e766b5e3
Joachim MARIE <moonstroke+github@live.fr>
2024-03-31 00:44:24 +0100

    Add test suites for methods load and store
    These suites also inherit from the base test class, and their test
    methods use the utility methods so that the test code is clear and
    immediately understandable.

---
+++ src/test/java/io/github/moonstroke/nprops/test/NPropsPropertiesLoadTest.java
@@ -0,0 +1,75 @@
+package io.github.moonstroke.nprops.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+class NPropsPropertiesLoadTest extends BaseNpropsPropertiesTest {
+
+	@Test
+	void testLoadNullInputStreamFails() {
+		assertThrows(NullPointerException.class, () -> properties.load((InputStream) null));
+	}
+
+	@Test
+	void testLoadOnePropertySucceeds() {
+		loadFromString("foo=bar");
+		assertEquals(properties.getProperty("foo"), "bar");
+	}
+
+	@Test
+	void testLoadIgnoresSurroundingWhitespace() {
+		loadFromString("\tfoo = bar\n");
+		assertEquals(properties.getProperty("foo"), "bar");
+	}
+
+	@Test
+	void testLoadFailsWithoutDelimiter() {
+		assertThrows(IllegalStateException.class, () -> loadFromString("foo"));
+	}
+
+	@Test
+	void testLoadFailsWithColonAsDelimiter() {
+		assertThrows(IllegalStateException.class, () -> loadFromString("foo: bar"));
+	}
+
+	@Test
+	void testLoadFailsOnNonEscapingBackslash() {
+		assertThrows(IllegalStateException.class, () -> loadFromString("foo = bar\\x"));
+	}
+
+	// TODO test all authorized backslash escapes
+
+	@Test
+	void testLoadIgnoresHashComments() {
+		loadFromString("foo = bar\n# comment\nbaz = quux");
+		assertEquals(properties.getProperty("foo"), "bar");
+		assertEquals(properties.getProperty("baz"), "quux");
+	}
+
+	@Test
+	void testLoadIgnoresBangComments() {
+		loadFromString("foo = bar\n! comment\nbaz = quux");
+		assertEquals(properties.getProperty("foo"), "bar");
+		assertEquals(properties.getProperty("baz"), "quux");
+	}
+
+
+	/* The following tests reference features deliberately *not* provided, but that might still be implemented in the
+	 * future */
+
+	@Test
+	void testLoadHasNoInlineComments() {
+		loadFromString("foo = bar # not a comment");
+		assertEquals(properties.getProperty("foo"), "bar # not a comment");
+	}
+
+	/* Special case of testLoadFailsOnNonEscapingBackslash */
+	@Test
+	void testLoadFailsOnUnicodeEscape() {
+		assertThrows(IllegalStateException.class, () -> loadFromString("foo = b\\u0061r"));
+	}
+}
