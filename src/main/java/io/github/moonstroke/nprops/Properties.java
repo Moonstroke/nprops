0e6eee0eb856d91eb3cea73d633205eeb5c1e720
Moonstroke <jiema@live.fr>
2024-04-28 00:34:13 +0200

    Document more the Properties class
    A description, hopefully thorough, is added for the features of the
    class, as well as an example proerty excerpt swith explicit corner
    cases mentioned.

--- src/main/java/io/github/moonstroke/nprops/Properties.java
+++ src/main/java/io/github/moonstroke/nprops/Properties.java
@@ -16,6 +16,86 @@ import java.io.Serializable;
  * {@link java.util.Properties} class, with at the same time some improvements
  * introduced.
  *
+ * <b>Definition of a property</b><br>
+ * Properties are key-value pairs, expressed in {@code key = value} form. The
+ * value can be empty, but not the key.
+ * <p>
+ * <b>Handling of whitespace characters</b><br>
+ * Horizontal tabulations or spaces are ignored before the key and around the
+ * separator as well as after the property value; the various platform-specific
+ * forms of line terminator characters at the end of the line are all accepted,
+ * even within a single property file.<br>
+ * Empty lines, or lines consisting only of whitespace characters, are
+ * accepted.
+ * <p>
+ * <b>Line wrapping</b><br>
+ * Values (or keys, although this is not advised) can be split to the next text
+ * line, by terminating the line with a single backslash character to indicate
+ * continuation. The leading whitespace on the next line is totally ignored (in
+ * particular, it is *not* replaced with a single space). This allows to align
+ * the continuation text vertically with the previous line without changing the
+ * content. Whitespace before the trailing backslash, on the other hand, is
+ * preserved.
+ * <p>
+ * <b>Escape sequences</b><br>
+ * An escape sequence is a combination of an escape character (the backslash)
+ * followed by a second character, standing for another character, usually one
+ * that is not easily printable or that has special meaning. Legal escape
+ * sequences are: {@code \n} (line-feed), {@code \t} (horizontal tabulation),
+ * {@code \r} (carriage return), {@code \\} (a backslash), {@code \f} (form
+ * feed). Some additional sequences are accepted: {@code \=} in the property
+ * key stands for a equals sign and removes its meaning of property delimiter.
+ * It is also allowed in the value; although this combination has no special
+ * meaning, it can convey more explicitly that this character is not meant as a
+ * delimiter. The sequence {@code \:} is also accepted in both key and value,
+ * even though the colon is not a special character in this implementation (it
+ * is not accepted as property delimiter); the sequence evaluates to the colon
+ * character itself. This feature is provided for compatibility with the
+ * standard {@link java.util.Properties} class. For the same reason, sequences
+ * {@code \'} and {@code \"} are accepted, and evaluate to the single and
+ * double quotes characters, respectively.<br>
+ * In contrast with the original class, an illegal escape sequence is not
+ * silently ignored, but an error is raised when parsing, to immediately report
+ * possible syntax oversights.
+ * <p>
+ * <b>Comments</b><br>
+ * Lines where the first non-whitespace character is a hash sign are considered
+ * comments, destined to be read by human users, and are ignored by the code.
+ * On the other hand, the exclamation mark is not considered a comment
+ * indicator: line starting with this character are parsed for a property like
+ * regular lines.<br>
+ * Comments are only considered such when starting from the beginning of a line
+ * (whitespace excluded), text after a hash sign at the end of a property
+ * definition is part of the property's value.
+ * <p>
+ * Here is an example of what a properties declaration looks like:
+ * <pre>
+ * <code>
+ * projectName=NProps
+ * className = io.github.moonstroke.nprops.Properties
+ * emptyPropertyValue=
+ * # This is a comment.
+ *     #This is too!
+ * !this is = not a comment
+ *     whitespace   = not significant
+ * key with spaces = value # still value
+ * # The value of this property is "multi-line property value":
+ * wrappedProperty = multi-\
+ *                   line \
+ *                   property \
+ *                   value
+ * # These are two distinct properties, because the trailing backslash is
+ * # escaped:
+ * windowsPath = C:\\Users\\Moonstroke\\dev\\nprops\\
+ * unixPath = /home/moonstroke/dev/nprops/
+ * # On the other hand, this trailing backslash is not escaped so the two lines
+ * # are a single property, with value
+ * # "C:\Users\Moonstroke\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Eclipse":
+ * longWindowsPath = C:\\Users\\Moonstroke\\AppData\\Roaming\\Microsoft\\Windows\\\
+ *     Start Menu\\Programs\\Eclipse
+ * </code>
+ * </pre>
+ *
  * @author Moonstroke
  */
 public class Properties implements Serializable {
